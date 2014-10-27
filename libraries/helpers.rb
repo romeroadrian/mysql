1eed35b52158a2813937ab1b37d497d5191513a6
Gustavo Giráldez <ggiraldez@manas.com.ar>
2014-10-27 14:33:06 -0300

    Add Amazon Linux 2014.09

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -96,6 +96,17 @@ module Opscode
               '5.6' => {
                 'package_name' => 'mysql-community-server'
               }
+            },
+            '2014.09' => {
+              '5.1' => {
+                'package_name' => 'mysql51-server'
+              },
+              '5.5' => {
+                'package_name' => 'mysql-community-server'
+              },
+              '5.6' => {
+                'package_name' => 'mysql-community-server'
+              }
             }
           },
           'fedora' => {
