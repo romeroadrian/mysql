10110e7f1697c7515096efc88db34f0aa53491e1
Adrian Romero <aromero@manas.com.ar>
2015-12-10 16:01:07 -0300

    Added Amazon Linux 2015.09

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -107,6 +107,17 @@ module Opscode
               '5.6' => {
                 'package_name' => 'mysql-community-server'
               }
+            },
+            '2015.09' => {
+              '5.1' => {
+                'package_name' => 'mysql51-server'
+              },
+              '5.5' => {
+                'package_name' => 'mysql-community-server'
+              },
+              '5.6' => {
+                'package_name' => 'mysql-community-server'
+              }
             }
           },
           'fedora' => {
